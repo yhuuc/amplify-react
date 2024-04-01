114b9eb4c8216d11b836a848a2e62cc4e63620dc
yhuuc <78474852+yhuuc@users.noreply.github.com>
2024-04-01 17:17:29 +1100

    updated title,h1,h2&color

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Song Popularity Predictor</h1>
+        <h2 className="App-subtitle">Billboard Ranking</h2>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
