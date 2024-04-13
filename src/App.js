1d6213d118ef4f7b9e7629d4bebfadd0403f8050
yhuuc <78474852+yhuuc@users.noreply.github.com>
2024-04-14 08:05:47 +1000

    Added simple dropzone

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import FileUpload from './FileUpload';
+
 
 function App() {
   return (
@@ -19,9 +21,11 @@ function App() {
         >
           Learn React
         </a>
+        <FileUpload />
       </header>
     </div>
   );
 }
+
 
 export default App;
