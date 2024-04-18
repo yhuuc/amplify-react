344d44958213297d4b99dbdcacab4a535b1a30c6
yhuuc <78474852+yhuuc@users.noreply.github.com>
2024-04-18 22:53:24 +1000

    Changed UI to black and white

--- src/App.js
+++ src/App.js
@@ -6,23 +6,10 @@ function App() {
   return (
     <div className="App">
         <header className="App-header">
-          <div id="Shadow">
-          <h1 className="App-title">Song Popularity Predictor</h1>
+          <div className="Shadow-box">
+            <h1 className="App-title">Song Popularity Predictor</h1>
           </div>
-          <h2 className="App-subtitle">Billboard Ranking</h2>
-          
-          {/* <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a> */}
+          <h2 className="App-subtitle">Based on <i>Billboard</i> ranking</h2>
         </header>
       <br></br>
         <body>
