0e1816252fe58804bfcf04c9a681904db77b6177
yhuuc <78474852+yhuuc@users.noreply.github.com>
2024-05-13 11:49:54 +1000

    removed unnecessary code

--- src/ShowButton.js
+++ src/ShowButton.js
@@ -11,26 +11,6 @@ function ShowButton() {
       //   // }
       //   visorInstance.toggle();
       // }
-
-    // const tensor = tf.tensor1d([0, 0, 0, 0, 2, 3, 4]);
-
-    // const surface1 = {name: 'Values Distribution', tab: 'Model Inspection'};
-    // await tfvis.show.valuesDistribution(surface1, tensor);
-    // // disable the button
-
-
-    // const model = tf.sequential({
-    //     layers: [
-    //       tf.layers.dense({inputShape: [784], units: 32, activation: 'relu'}),
-    //       tf.layers.dense({units: 10, activation: 'softmax'}),
-    //     ]
-    //   });
-      
-    //   const surface2 = { name: 'Layer Summary', tab: 'Model Inspection'};
-    //   tfvis.show.layer(surface2, model.getLayer(undefined, 1));
-
-    //   const surface3 = { name: 'Model Summary', tab: 'Model Inspection'};
-    //   tfvis.show.modelSummary(surface3, model);
   };
 
   return (
